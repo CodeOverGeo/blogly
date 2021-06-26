fe9759005ac74ba342a4ade75e81902615cb9987
Giovanni Rufino <giovannirufino@gmail.com>
2021-06-26 10:52:41 -0400

    Finished all routes and fixed templates, html and static images

--- app.py
+++ app.py
@@ -1,37 +1,102 @@
 """Blogly application."""
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 app.config['SECRET_KEY'] = "Hamilton"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-debug = DebugToolbarExtension(app)
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def root():
+    """Root route redirects to user list"""
+    return redirect ('/users')
 
 @app.route('/get')
 def get():
     pass
 
 @app.route('/users')
-def user():
-    return render_template ('users.html')
+def users():
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template ('users/users.html', users=users)
 
-@app.route('/new_user')
-def new_user():
-    pass
+@app.route('/users/new')
+def new_user_page():
+    """Show a form to create a new user"""
 
-@app.route('/user_detail')
-def user_detail():
-    return render_template ('user_detail.html')
+    return render_template('users/new.html')
 
-@app.route('/edit_user')
-def edit_user():
-    pass
+@app.route('/users/new', methods=['POST'])
+def create_user():
+    """Get form values from new user page and create a new user"""
+
+    new_user = User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form.get('image_url') or None
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.route('/user/<int:user_id>')
+def user_show(user_id):
+    """Show detail page for specific user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/detail.html', user = user)
+
+
+@app.route('/users/<int:user_id>')
+def user_detail(user_id):
+
+    user = User.query.get_or_404(user_id)
+    return render_template ('users/detail.html', user = user)
+
+@app.route('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    """Show a form to edit a specific user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/edit.html', user = user)
+
+@app.route('/users/<int:user_id>/edit', methods = ['POST'])
+def update_user(user_id):
+    """Get edit user form values and update database"""
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form.get('image_url') or None
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.route('/user/<int:user_id>/delete', methods=['POST'])
+def user_delete(user_id):
+    """Deletes user based off of delete request"""
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect('/users')
+
+
+
+
+    
--- models.py
+++ models.py
@@ -7,13 +7,14 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+DEFAULT_IMAGE_URL = "/static/generic_profile.png"
 
 class User(db.Model):
     """User"""
 
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    id = db.Column(db.Integer, primary_key = True)
     
     first_name = db.Column(db.String(30),
                         nullable = False)
@@ -21,4 +22,10 @@ class User(db.Model):
     last_name = db.Column(db.String(30),
                         nullable = False)
 
-    image_url = db.Column(db.String, default = "/pics/generic_profile.png")
+    image_url = db.Column(db.String, nullable = False, default = DEFAULT_IMAGE_URL)
+
+    @property
+    def full_name(self):
+        """Return full name of user."""
+
+        return f"{self.first_name} {self.last_name}"
